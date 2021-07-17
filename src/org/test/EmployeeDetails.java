1e511444673713f39c2714e265a4ca364d81ec67
AbuThurab <abuanish4@gmail.com>
2021-07-17 08:07:20 +0530

    Code Added

---
+++ src/org/test/EmployeeDetails.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class EmployeeDetails {
+	public static void main(String[] args) {
+		int emp = 123;
+		
+		if (emp>10) {
+			System.out.println("valid ID");
+			
+		}
+		else {
+			throw new Employee NotFound Exception();
+		}
+	}
+
+}
